942d7c81a6a1217dd3609968c22ece54e85a858e
mercury233 <me@mercury233.me>
2023-08-15 15:11:47 +0800

    another

--- test.lua
+++ test.lua
@@ -4,6 +4,11 @@ dog1:SetOnPetting(dogBark)
 local dog2 = DOG.CreateDog()
 dog2:SetOnPetting(dogMeow)
 
+local dog3 = DOG.CreateDog()
+dog3:SetOnPetting(function (power, say)
+	print("dogPurr", power, say)
+end)
+
 function dogBark(power, say)
 	print("dogBark", power, say)
 end
